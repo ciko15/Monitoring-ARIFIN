128e790cc77810e1d4ce86428637761edbacbd60
ciko15 <vickraanugrah@gmail.com>
2026-06-30 15:13:01 +0700

    Add trigger to dump_hex.js

--- dump_hex.js
+++ dump_hex.js
@@ -1,15 +1,19 @@
 const net = require('net');
 const client = new net.Socket();
 
+const TRIGGER_SEND = Buffer.from([0x0B, 0x00, 0xF9, 0x06]);
+
 client.connect(950, '192.168.51.10', () => {
     console.log('[+] Connected to 192.168.51.10:950');
+    console.log('[+] Mengirim Trigger Request...');
+    client.write(TRIGGER_SEND);
 });
 
 client.on('data', (data) => {
     console.log('\n--- RAW HEX DUMP ---');
     console.log(data.toString('hex'));
     console.log('--------------------\n');
-    client.destroy(); // Langsung putus setelah dapat 1 chunk
+    client.destroy(); 
 });
 
 client.on('error', (err) => {
