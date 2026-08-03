08b78fbc5aa46fb471f8d7855e5e9ac94eca4bc6
AHQMB-TT017 <ahqmb-tt017@AHQMB-TT017s-MacBook-Air.local>
2026-08-03 23:52:51 +0800

    fix test-humidity

--- test-humidity.js
+++ test-humidity.js
@@ -1,7 +1,7 @@
 const ModbusRTU = require("modbus-serial");
 
 // Konfigurasi IP dan Port (Silakan ubah port di sini)
-const HOST = "172.19.7.160";
+const HOST = "192.168.0.26";
 const PORT = 8899; // <--- Ubah port sesuai keinginan Anda
 const SLAVE_ID = 1;
 
@@ -9,22 +9,22 @@ async function testHumidity() {
     const client = new ModbusRTU();
     try {
         console.log(`🔌 Menghubungkan ke Sensor Suhu & Kelembapan (${HOST}:${PORT})...`);
-        client.setTimeout(3000); 
+        client.setTimeout(3000);
         await client.connectTelnet(HOST, { port: PORT });
-        
-        client.setID(SLAVE_ID); 
+
+        client.setID(SLAVE_ID);
         console.log(`✅ Terhubung ke Slave ID ${SLAVE_ID}\n`);
 
         console.log("🌡️  MEMBACA DATA SENSOR SUHU & KELEMBAPAN");
         console.log("-----------------------------------------");
-        
+
         // Membaca Input Register alamat 1 dan 2
         const res = await client.readInputRegisters(1, 2);
-        
+
         // Asumsi data perlu dibagi 10 (misal raw 231 = 23.1)
         const suhu = (res.data[0] / 10).toFixed(1);
         const humidity = (res.data[1] / 10).toFixed(1);
-        
+
         console.log(`✅ Suhu Ruangan       : ${suhu} °C`);
         console.log(`✅ Kelembapan         : ${humidity} %`);
         console.log("-----------------------------------------");
